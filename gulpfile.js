f9782ba91cca2814ab30b01259ae24691616f8ae
Travis Webb <me@traviswebb.com>
2015-07-13 00:01:32 -0400

    build es6 on prepublish

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 gulp.task('build', function () {
-  return gulp.src([ 'lib/**' ])
+  return gulp.src([ 'api/**' ])
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
